e04e3867c3c03f7fe5b6f55b132a783d45e69252
greedysky <greedysky@163.com>
2015-09-11 20:03:16 +0800

    integrated enum configtype using QMetaEnum[695120]

--- core/musicsettingmanager.h
+++ core/musicsettingmanager.h
@@ -10,6 +10,7 @@
    =================================================*/
 
 #include "musicsingletone.h"
+#include <QMetaEnum>
 
 #define M_SETTING (MusicSingleton<MusicSettingManager>::createInstance())
 
@@ -113,58 +114,10 @@ protected:
     MusicSettingManager(){}
     ConfigType typeStringToEnum(const QString &stype) const
     {
-        ConfigType type = Null;
-        if(stype == "PlayModeChoiced") type = PlayModeChoiced;
-        if(stype == "VolumeChoiced") type = VolumeChoiced;
-        if(stype == "AutoPlayChoiced") type = AutoPlayChoiced;
-        if(stype == "Play3DMusicChoiced") type = Play3DMusicChoiced;
-        if(stype == "CurrentLanIndexChoiced") type = CurrentLanIndexChoiced;
-        if(stype == "CloseEventChoiced") type = CloseEventChoiced;
-        if(stype == "LastPlayIndexChoiced") type = LastPlayIndexChoiced;
-        if(stype == "BgThemeChoiced") type = BgThemeChoiced;
-        if(stype == "BgTransparentChoiced") type = BgTransparentChoiced;
-
-        if(stype == "ShowInlineLrcChoiced") type = ShowInlineLrcChoiced;
-        if(stype == "ShowDesktopLrcChoiced") type = ShowDesktopLrcChoiced;
-        if(stype == "LrcColorChoiced") type = LrcColorChoiced;
-        if(stype == "LrcSizeChoiced") type = LrcSizeChoiced;
-        if(stype == "LrcTypeChoiced") type = LrcTypeChoiced;
-        if(stype == "LrcFamilyChoiced") type = LrcFamilyChoiced;
-        if(stype == "LrcFgColorChoiced") type = LrcFgColorChoiced;
-        if(stype == "LrcBgColorChoiced") type = LrcBgColorChoiced;
-        if(stype == "LrcColorTransChoiced") type = LrcColorTransChoiced;
-        if(stype == "DLrcTypeChoiced") type = DLrcTypeChoiced;
-        if(stype == "DLrcColorChoiced") type = DLrcColorChoiced;
-        if(stype == "DLrcSizeChoiced") type = DLrcSizeChoiced;
-        if(stype == "DLrcFamilyChoiced") type = DLrcFamilyChoiced;
-        if(stype == "DLrcFgColorChoiced") type = DLrcFgColorChoiced;
-        if(stype == "DLrcBgColorChoiced") type = DLrcBgColorChoiced;
-        if(stype == "DLrcColorTransChoiced") type = DLrcColorTransChoiced;
-        if(stype == "DLrcLockedChoiced") type = DLrcLockedChoiced;
-        if(stype == "DLrcGeometryChoiced") type = DLrcGeometryChoiced;
-
-        if(stype == "EqualizerEnableChoiced") type = EqualizerEnableChoiced;
-        if(stype == "EqualizerValueChoiced") type = EqualizerValueChoiced;
-        if(stype == "EqualizerIndexChoiced") type = EqualizerIndexChoiced;
-        if(stype == "SequalizerIndexChoiced") type = SequalizerIndexChoiced;
-
-        if(stype == "TimerAutoIndexChoiced") type = TimerAutoIndexChoiced;
-        if(stype == "TimerAutoPlayChoiced") type = TimerAutoPlayChoiced;
-        if(stype == "TimerAutoPlayHourChoiced") type = TimerAutoPlayHourChoiced;
-        if(stype == "TimerAutoPlaySecondChoiced") type = TimerAutoPlaySecondChoiced;
-        if(stype == "TimerAutoPlayRepeatChoiced") type = TimerAutoPlayRepeatChoiced;
-        if(stype == "TimerAutoPlayItemIndexChoiced") type = TimerAutoPlayItemIndexChoiced;
-        if(stype == "TimerAutoPlaySongIndexChoiced") type = TimerAutoPlaySongIndexChoiced;
-        if(stype == "TimerAutoStopChoiced") type = TimerAutoStopChoiced;
-        if(stype == "TimerAutoStopHourChoiced") type = TimerAutoStopHourChoiced;
-        if(stype == "TimerAutoStopSecondChoiced") type = TimerAutoStopSecondChoiced;
-        if(stype == "TimerAutoStopRepeatChoiced") type = TimerAutoStopRepeatChoiced;
-        if(stype == "TimerAutoShutdownChoiced") type = TimerAutoShutdownChoiced;
-        if(stype == "TimerAutoShutdownHourChoiced") type = TimerAutoShutdownHourChoiced;
-        if(stype == "TimerAutoShutdownSecondChoiced") type = TimerAutoShutdownSecondChoiced;
-        if(stype == "TimerAutoShutdownRepeatChoiced") type = TimerAutoShutdownRepeatChoiced;
-
-        return type;
+        int index = staticMetaObject.indexOfEnumerator("ConfigType");
+        QMetaEnum metaEnum = staticMetaObject.enumerator(index);
+        int key = metaEnum.keyToValue(stype.toStdString().c_str());
+        return static_cast<ConfigType>(key);
     }
 
     QVariant m_variant;
