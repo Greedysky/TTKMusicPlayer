835f1eb9a758d76a42d1505782c728f402277bc7
greedysky <greedysky@163.com>
2015-09-16 13:19:51 +0800

    update MusicSongsListItemInfoWidget elidedText[562316]

--- core-widget/musicsongslistiteminfowidget.cpp
+++ core-widget/musicsongslistiteminfowidget.cpp
@@ -35,11 +35,16 @@ void MusicSongsListItemInfoWidget::setMusicSongInformation(MusicSong &info)
     QString musicArt = info.getMusicArtistFront();
     QFontMetrics str(font());
     ui->songNameValue->setText( info.getMusicName().isEmpty() ? "-" :
-                 str.elidedText( info.getMusicName(), Qt::ElideRight, ui->songNameValue->width()) );
-    ui->artlistValue->setText( musicArt.isEmpty() ? "-" : musicArt );
-    ui->sizeValue->setText( QString::number(MusicTime::fileSzieByte2MByte(info.getMusicSize())).left(4) + "M" );
-    ui->typeValue->setText( info.getMusicType().isEmpty() ? "-" : info.getMusicType());
-    ui->timeValue->setText( QString::number(info.getMusicPlayCount()) );
+                str.elidedText( info.getMusicName(), Qt::ElideRight, ui->songNameValue->width()) );
+    ui->artlistValue->setText( musicArt.isEmpty() ? "-" :
+               str.elidedText( musicArt, Qt::ElideRight, ui->artlistValue->width()) );
+    ui->sizeValue->setText( str.elidedText( QString::number(MusicTime::fileSzieByte2MByte(
+                            info.getMusicSize())).left(4) + "M", Qt::ElideRight, ui->sizeValue->width()) );
+    ui->typeValue->setText( info.getMusicType().isEmpty() ? "-" :
+            str.elidedText( info.getMusicType(), Qt::ElideRight, ui->typeValue->width()) );
+    ui->timeValue->setText(
+            str.elidedText( QString::number(info.getMusicPlayCount()), Qt::ElideRight, ui->timeValue->width()) );
+
 
     if(!showArtPicture(musicArt) && !showArtPicture(info.getMusicArtistBack()))
     {
