cbc3636e9438e41040c16f7d682a96d2527b0bed
greedysky <greedysky@163.com>
2015-09-23 10:44:18 +0800

    simplify remote Download List[345626]

--- core-widget/musicbackgroundremotewidget.cpp
+++ core-widget/musicbackgroundremotewidget.cpp
@@ -78,7 +78,7 @@ void MusicBackgroundRemoteWidget::buttonClicked(int index)
         case 3: count = 9; break;
     }
 
-    QString path = QString("%1").arg(m_currentIndex);
+    QString path = QString("%1%2").arg(THEME_CACHED).arg(m_currentIndex);
     QDir dir( path );
     if(!dir.exists())
     {
@@ -106,77 +106,78 @@ void MusicBackgroundRemoteWidget::itemUserClicked(QListWidgetItem *item)
 
 void MusicBackgroundRemoteWidget::createUrls()
 {
+    QString prefix = "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/";
     ////////////////////1
     m_urls << (QStringList()
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706532786_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706533257_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706531729_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706530884_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706530322_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706529683_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706772382_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706773800_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706857712_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392706858550_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707046854_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707043813_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707042612_0.jpg")
+           << prefix + "1392706532786_0.jpg"
+           << prefix + "1392706533257_0.jpg"
+           << prefix + "1392706531729_0.jpg"
+           << prefix + "1392706530884_0.jpg"
+           << prefix + "1392706530322_0.jpg"
+           << prefix + "1392706529683_0.jpg"
+           << prefix + "1392706772382_0.jpg"
+           << prefix + "1392706773800_0.jpg"
+           << prefix + "1392706857712_0.jpg"
+           << prefix + "1392706858550_0.jpg"
+           << prefix + "1392707046854_0.jpg"
+           << prefix + "1392707043813_0.jpg"
+           << prefix + "1392707042612_0.jpg")
     ////////////////////2
            << (QStringList()
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392710159240_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392710158654_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716523048_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716523976_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716641276_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716644700_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716646260_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716647355_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716642799_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716685115_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716688192_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716690865_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716691391_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392716691992_0.jpg")
+           << prefix + "1392710159240_0.jpg"
+           << prefix + "1392710158654_0.jpg"
+           << prefix + "1392716523048_0.jpg"
+           << prefix + "1392716523976_0.jpg"
+           << prefix + "1392716641276_0.jpg"
+           << prefix + "1392716644700_0.jpg"
+           << prefix + "1392716646260_0.jpg"
+           << prefix + "1392716647355_0.jpg"
+           << prefix + "1392716642799_0.jpg"
+           << prefix + "1392716685115_0.jpg"
+           << prefix + "1392716688192_0.jpg"
+           << prefix + "1392716690865_0.jpg"
+           << prefix + "1392716691391_0.jpg"
+           << prefix + "1392716691992_0.jpg")
     ////////////////////3
            << (QStringList()
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392630277898_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392630274891_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392630274257_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392630272320_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392630269669_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633744378_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633739646_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633739218_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633734129_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633472586_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633476617_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633475965_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633460419_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633462485_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633465121_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633040098_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633042823_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633043634_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633034581_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392633017171_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632216085_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632215251_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632211699_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632209023_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632031561_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632022038_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392632018561_0.jpg")
+           << prefix + "1392630277898_0.jpg"
+           << prefix + "1392630274891_0.jpg"
+           << prefix + "1392630274257_0.jpg"
+           << prefix + "1392630272320_0.jpg"
+           << prefix + "1392630269669_0.jpg"
+           << prefix + "1392633744378_0.jpg"
+           << prefix + "1392633739646_0.jpg"
+           << prefix + "1392633739218_0.jpg"
+           << prefix + "1392633734129_0.jpg"
+           << prefix + "1392633472586_0.jpg"
+           << prefix + "1392633476617_0.jpg"
+           << prefix + "1392633475965_0.jpg"
+           << prefix + "1392633460419_0.jpg"
+           << prefix + "1392633462485_0.jpg"
+           << prefix + "1392633465121_0.jpg"
+           << prefix + "1392633040098_0.jpg"
+           << prefix + "1392633042823_0.jpg"
+           << prefix + "1392633043634_0.jpg"
+           << prefix + "1392633034581_0.jpg"
+           << prefix + "1392633017171_0.jpg"
+           << prefix + "1392632216085_0.jpg"
+           << prefix + "1392632215251_0.jpg"
+           << prefix + "1392632211699_0.jpg"
+           << prefix + "1392632209023_0.jpg"
+           << prefix + "1392632031561_0.jpg"
+           << prefix + "1392632022038_0.jpg"
+           << prefix + "1392632018561_0.jpg")
     ////////////////////4
            << (QStringList()
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707254886_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707296538_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707322411_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707398965_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707420038_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707447194_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707472146_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707690215_0.jpg"
-           << "http://img1.kwcdn.kuwo.cn/star/KuwoPhotoArt/0/0/1392707678198_0.jpg");
+           << prefix + "1392707254886_0.jpg"
+           << prefix + "1392707296538_0.jpg"
+           << prefix + "1392707322411_0.jpg"
+           << prefix + "1392707398965_0.jpg"
+           << prefix + "1392707420038_0.jpg"
+           << prefix + "1392707447194_0.jpg"
+           << prefix + "1392707472146_0.jpg"
+           << prefix + "1392707690215_0.jpg"
+           << prefix + "1392707678198_0.jpg");
 }
 
 QStringList MusicBackgroundRemoteWidget::createPaths()
