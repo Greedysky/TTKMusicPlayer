55ffbe77194e31ae73b0fae920fc9bfa0e39317c
greedysky <greedysky@163.com>
2015-09-24 11:34:37 +0800

    support global log on[542165]

--- musicapplicationmain.cpp
+++ musicapplicationmain.cpp
@@ -2,7 +2,7 @@
 #include "musicobject.h"
 #include "musicxmlconfigmanager.h"
 #include "musicnetworkthread.h"
-//#include "musiclogger.h"
+#include "musiclogger.h"
 
 #include <QDesktopWidget>
 #include <QApplication>
@@ -13,10 +13,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    M_LOOGERS("MusicApplication Begin");
     QCoreApplication::setOrganizationName("QMusicPlayer");
     QCoreApplication::setOrganizationDomain("QMusicPlayer.com");
     QCoreApplication::setApplicationName("QMusicPlayer");
 
+    M_LOOGERS("Load Translation");
     MusicXMLConfigManager *xml = new MusicXMLConfigManager;
     xml->readXMLConfig();
 
@@ -24,12 +26,12 @@ int main(int argc, char *argv[])
     translator.load(MusicObject::getLanguageName(xml->readLanguageIndex()));
     a.installTranslator(&translator);
     delete xml;
+    M_LOOGERS("End load translation");
 
     //detect the current network state
     M_NETWORK->start();
 
     MusicObject::checkTheDirectoryExist();
-//    M_LOOGER("MusicApplication Begin");
     MusicApplication w;
     w.show();
 
