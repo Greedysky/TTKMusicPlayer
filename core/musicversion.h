d6f3e55b7618cf0f5c33627c06789370c1db1220
greedysky <greedysky@163.com>
2015-10-04 09:43:44 +0800

    Release version at 2.2.5.6

--- core/musicversion.h
+++ core/musicversion.h
@@ -9,7 +9,7 @@
  * works are strictly forbiden.
    =================================================*/
 
-#define QMUSICPLAYER_VERSION "Version 2.2.5.4"
+#define QMUSICPLAYER_VERSION "Version 2.2.5.6"
 
 #endif // MUSICVERSION
 
