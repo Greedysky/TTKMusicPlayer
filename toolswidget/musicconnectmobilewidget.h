fd70ab9d8ea13c930fddbde7b1b9846f444069fb
greedysky <greedysky@163.com>
2015-10-15 12:43:49 +0800

    update music connecttion widget[124598]

--- toolswidget/musicconnectmobilewidget.h
+++ toolswidget/musicconnectmobilewidget.h
@@ -4,18 +4,28 @@
 #include <QWidget>
 #include "musiclibexportglobal.h"
 
+class QStackedWidget;
+
 class MUSIC_TOOL_EXPORT MusicConnectMobileWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit MusicConnectMobileWidget(QWidget *parent = 0);
+    ~MusicConnectMobileWidget();
 
 signals:
 
 public slots:
+    void changeStatckedWidgetFirst();
+    void changeStatckedWidgetSecond();
+    void changeStatckedWidgetThird();
 
 protected:
+    void initFirstWidget();
+    void initSecondWidget();
+    void initThirdWidget();
 
+    QStackedWidget *m_stackedWidget;
 
 };
 
