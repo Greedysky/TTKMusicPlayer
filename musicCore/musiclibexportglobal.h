018dd7e1dd445ebe21106b631e737c358f2c10e7
greedysky <greedysky@163.com>
2015-11-23 21:35:43 +0800

    update libexport to global[003590]

--- musicCore/musiclibexportglobal.h
+++ musicCore/musiclibexportglobal.h
@@ -11,6 +11,8 @@
 
 #include <QtCore/qglobal.h>
 
+#define MUSIC_EXPORT
+
 #ifdef QT_DEBUG
 #   define MUSIC_DEBUG
 #endif
@@ -31,69 +33,101 @@
 
 
 #ifdef MUSIC_GUI
-#  define MUSIC_GUI_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_GUI_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_GUI_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_GUI_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_NETWORK
-#  define MUSIC_NETWORK_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_NETWORK_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_NETWORK_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_NETWORK_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_CORE
-#  define MUSIC_CORE_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_CORE_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_CORE_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_CORE_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_WIDGET
-#  define MUSIC_WIDGET_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_WIDGET_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_WIDGET_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_WIDGET_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_SEARCH
-#  define MUSIC_SEARCH_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_SEARCH_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_SEARCH_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_SEARCH_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_LRC
-#  define MUSIC_LRC_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_LRC_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_LRC_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_LRC_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_REMOTE
-#  define MUSIC_REMOTE_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_REMOTE_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_REMOTE_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_REMOTE_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_TOOL
-#  define MUSIC_TOOL_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_TOOL_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_TOOL_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_TOOL_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_TOOLSET
-#  define MUSIC_TOOLSET_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_TOOLSET_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_TOOLSET_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_TOOLSET_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
+
 
 #ifdef MUSIC_USER
-#  define MUSIC_USER_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_USER_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_USER_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_USER_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
 
+
 #ifdef MUSIC_VIDEO
-#  define MUSIC_VIDEO_EXPORT Q_DECL_EXPORT
-#else
-#  define MUSIC_VIDEO_IMPORT Q_DECL_IMPORT
+#  ifdef MUSIC_EXPORT
+#    define MUSIC_VIDEO_EXPORT Q_DECL_EXPORT
+#  else
+#    define MUSIC_VIDEO_IMPORT Q_DECL_IMPORT
+#  endif
 #endif
 
 ///load logger
