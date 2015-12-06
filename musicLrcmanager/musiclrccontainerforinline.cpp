b45c2e67f08784074a623767fc285f662315bbbc
greedysky <greedysky@163.com>
2015-12-06 21:14:41 +0800

    update context menu stylesheet[645320]

--- musicLrcmanager/musiclrccontainerforinline.cpp
+++ musicLrcmanager/musiclrccontainerforinline.cpp
@@ -356,9 +356,9 @@ void MusicLrcContainerForInline::contextMenuEvent(QContextMenuEvent *)
     QMenu menu(this);
     QMenu changColorMenu(tr("changColorMenu"),this);
     QMenu changeLrcSize(tr("changeLrcSize"),this);
-    changColorMenu.setStyleSheet(MusicUIObject::MMenuStyle01);
-    changeLrcSize.setStyleSheet(MusicUIObject::MMenuStyle01);
-    menu.setStyleSheet(MusicUIObject::MMenuStyle01);
+    changColorMenu.setStyleSheet(MusicUIObject::MMenuStyle02);
+    changeLrcSize.setStyleSheet(MusicUIObject::MMenuStyle02);
+    menu.setStyleSheet(MusicUIObject::MMenuStyle02);
     menu.addAction(tr("searchLrcs"), this, SLOT(searchMusicLrcs()));
     menu.addAction(tr("updateLrc"), this, SIGNAL(theCurrentLrcUpdated()));
     menu.addAction(tr("makeLrc"), this, SLOT(theCurrentLrcMaked()));
