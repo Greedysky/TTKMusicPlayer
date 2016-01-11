f3f3f0a8b428d035d33250c332beeafcccd2007d
greedysky <greedysky@163.com>
2016-01-11 10:31:36 +0800

    add more music search url[402316]

--- musicCore/network/musicdownloadthreadabstract.h
+++ musicCore/network/musicdownloadthreadabstract.h
@@ -25,6 +25,16 @@ const QString MUSIC_LRC_URL = "http://lp.music.ttpod.com/lrc/down?lrcid=&artist=
 const QString SML_BG_ART_URL = "http://lp.music.ttpod.com/pic/down?artist=%1";
 const QString BIG_BG_ART_URL = "http://www.kuwo.cn/mingxing/%1/pic.htm";
 
+const QString MUSIC_REQUERY_WY = "http://api.musicuu.com/music/search/wy/%1/1?format=json"; //wangyiMusic
+const QString MUSIC_REQUERY_DX = "http://api.musicuu.com/music/search/dx/%1/1?format=json"; //dianxinMusic
+const QString MUSIC_REQUERY_QQ = "http://api.musicuu.com/music/search/qq/%1/1?format=json"; //qqMusic
+const QString MUSIC_REQUERY_XM = "http://api.musicuu.com/music/search/xm/%1/1?format=json"; //xiamiMusic
+const QString MUSIC_REQUERY_TT = "http://api.musicuu.com/music/search/tt/%1/1?format=json"; //ttpodMusic
+const QString MUSIC_REQUERY_BD = "http://api.musicuu.com/music/search/bd/%1/1?format=json"; //baiduMusic
+const QString MUSIC_REQUERY_KW = "http://api.musicuu.com/music/search/kw/%1/1?format=json"; //kuwoMusic
+const QString MUSIC_REQUERY_KG = "http://api.musicuu.com/music/search/kg/%1/1?format=json"; //kuhouMusic
+const QString MUSIC_REQUERY_DM = "http://api.musicuu.com/music/search/dm/%1/1?format=json"; //duomiMusic
+
 
 class MUSIC_NETWORK_EXPORT MusicDownLoadThreadAbstract : public QObject
 {
