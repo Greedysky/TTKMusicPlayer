8b0fd0789e91bc9508812374ccf247acfedd2042
greedysky <greedysky@163.com>
2016-02-17 14:27:14 +0800

    add make ts translation on executing CMake[885479]

---
+++ TTKScript/win.bat
@@ -0,0 +1,19 @@
+@echo off & setlocal EnableDelayedExpansion
+PUSHD %~dp0
+
+cd /D ../TTKLanguage
+set LCEXE="%1"
+set OUTPUT="%2/MLanguage"
+md %OUTPUT%
+::make
+for /F %%f in ('dir /A:A /s /b *.ts') do (
+  %LCEXE% %%f -silent -nounfinished
+)
+::rename
+ren *.qm *.ln
+::move
+for /F %%f in ('dir /A:A /s /b *.ln') do (
+  move %%f %OUTPUT%
+)
+
+POPD %~dp0
