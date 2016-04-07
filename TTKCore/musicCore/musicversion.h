621c134413636c2c1d4fd00ec001d0d1206b5075
greedysky <greedysky@163.com>
2016-04-07 10:38:34 +0800

    Release on 2.2.9.5

--- TTKCore/musicCore/musicversion.h
+++ TTKCore/musicCore/musicversion.h
@@ -9,15 +9,15 @@
  * works are strictly forbiden.
    =================================================*/
 
-//update time 2016.04.01
-#define TTKMUSIC_VERSION_STR "2.2.9.4"
+//update time 2016.04.07
+#define TTKMUSIC_VERSION_STR "2.2.9.5"
 
 #define TTKMUSIC_MAJOR_VERSION 2
 #define TTKMUSIC_MIDLE_VERSION 2
 #define TTKMUSIC_MINOR_VERSION 9
-#define TTKMUSIC_PATCH_VERSION 4
+#define TTKMUSIC_PATCH_VERSION 5
 
-#define TTKMUSIC_VERSION 0x02294
+#define TTKMUSIC_VERSION 0x02295
 
 #define TTKMUSIC_VERSION_CHECK(major, middle, minor, patch) ((major<<12)|(middle<<8)|(minor<<4)|(patch))
 
--- TTKMusicPlayer.pri
+++ TTKMusicPlayer.pri
@@ -79,7 +79,7 @@ unix:!mac{
     LIBS += -L../lib -lqmmp
 }
 
-TTKMusicPlayer = 2.2.9.4
+TTKMusicPlayer = 2.2.9.5
 
 DEFINES += MUSIC_LIBRARY
 DEFINES += USE_MULTIPLE_QUERY
