e3e8bfaca74ad55d8bb98e8343b21d65850bab48
greedysky <greedysky@163.com>
2016-04-15 13:03:55 +0800

    update analysis resource[486332]

--- TTKCore/musicToolsetswidget/musicvolumegainwidget.cpp
+++ TTKCore/musicToolsetswidget/musicvolumegainwidget.cpp
@@ -16,6 +16,36 @@ MusicVolumeGainWidget::MusicVolumeGainWidget(QWidget *parent)
     ui->topTitleCloseButton->setToolTip(tr("Close"));
     connect(ui->topTitleCloseButton, SIGNAL(clicked()), SLOT(close()));
 
+    ui->addFileButton->setIcon(QIcon(":/toolSets/gainAddFile"));
+    ui->addFileButton->setIconSize(QSize(40, 40));
+    ui->addFileButton->setStyleSheet(MusicUIObject::MToolButtonStyle03);
+    ui->addFileButton->setToolTip(tr("addFile"));
+
+    ui->addFilesButton->setIcon(QIcon(":/toolSets/gainAddFiles"));
+    ui->addFilesButton->setIconSize(QSize(40, 40));
+    ui->addFilesButton->setStyleSheet(MusicUIObject::MToolButtonStyle03);
+    ui->addFilesButton->setToolTip(tr("addFiles"));
+
+    ui->rmFileButton->setIcon(QIcon(":/toolSets/gainRmFile"));
+    ui->rmFileButton->setIconSize(QSize(40, 40));
+    ui->rmFileButton->setStyleSheet(MusicUIObject::MToolButtonStyle03);
+    ui->rmFileButton->setToolTip(tr("rmFile"));
+
+    ui->rmFilesButton->setIcon(QIcon(":/toolSets/gainRmFiles"));
+    ui->rmFilesButton->setIconSize(QSize(40, 40));
+    ui->rmFilesButton->setStyleSheet(MusicUIObject::MToolButtonStyle03);
+    ui->rmFilesButton->setToolTip(tr("rmFiles"));
+
+    ui->analysisButton->setIcon(QIcon(":/toolSets/analysis"));
+    ui->analysisButton->setIconSize(QSize(40, 40));
+    ui->analysisButton->setStyleSheet(MusicUIObject::MToolButtonStyle03);
+    ui->analysisButton->setToolTip(tr("analysis"));
+
+    ui->applyButton->setIcon(QIcon(":/toolSets/apply"));
+    ui->applyButton->setIconSize(QSize(50, 50));
+    ui->applyButton->setStyleSheet(MusicUIObject::MToolButtonStyle03);
+    ui->applyButton->setToolTip(tr("apply"));
+
 }
 
 MusicVolumeGainWidget::~MusicVolumeGainWidget()
