cf591fe407be974bafa295359f44638e3a1402f3
greedysky <greedysky@163.com>
2016-05-15 12:20:31 +0800

    update class commits[632485]

--- TTKCore/musicLrcmanager/musiclrcfromkrc.h
+++ TTKCore/musicLrcmanager/musiclrcfromkrc.h
@@ -11,23 +11,44 @@
 
 #include "musicglobaldefine.h"
 
-/*! @brief The class of the krc 2 lrc.
+/*! @brief The class of the krc to lrc.
  * @author Greedysky <greedysky@163.com>
  */
 class MUSIC_LRC_EXPORT MusicLrcFromKrc
 {
 public:
     MusicLrcFromKrc();
+    /*!
+     * Object contsructor.
+     */
     ~MusicLrcFromKrc();
 
     bool decode(const QString &input, const QString &output = QString());
+    /*!
+     * Decode krc file to lrc by input file and output file.
+     */
     QByteArray getDecodeString() const;
+    /*!
+     * Get decode string.
+     */
 
 protected:
     int sncasecmp(char *s1, char *s2, size_t n);
+    /*!
+     * Sncasecmp by input and size n.
+     */
     int decompression(unsigned char *src, size_t srcsize, size_t *dstsize);
+    /*!
+     * Decompress the krc file data to normal data.
+     */
     int isfilter(char *tok);
+    /*!
+     * Check the input string is filtered or not.
+     */
     void createLrc(unsigned char *lrc, int lrclen);
+    /*!
+     * Create lrc by input data and length.
+     */
 
     uchar *m_resultBytes;
     QByteArray m_data;
