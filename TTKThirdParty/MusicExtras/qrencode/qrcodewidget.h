b5f8ccdba479b435073f91379b662693754683f4
greedysky <greedysky@163.com>
2016-05-30 15:28:05 +0800

    update application copyright[987545]

--- TTKThirdParty/MusicExtras/qrencode/qrcodewidget.h
+++ TTKThirdParty/MusicExtras/qrencode/qrcodewidget.h
@@ -1,6 +1,14 @@
 #ifndef QRCODEWIDGET_H
 #define QRCODEWIDGET_H
 
+/* =================================================
+ * This file is part of the TTK Music Player project
+ * Copyright (c) 2014 - 2016 Greedysky Studio
+ * All rights reserved!
+ * Redistribution and use of the source code or any derivative
+ * works are strictly forbiden.
+   =================================================*/
+
 #include <QWidget>
 #include "qrencode.h"
 #include "musicextrasglobaldefine.h"
