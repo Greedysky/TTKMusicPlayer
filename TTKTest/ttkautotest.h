c9774123f1233e445cc5313e4695adc364f61869
greedysky <greedysky@163.com>
2016-06-17 11:42:47 +0800

    clean code[766589]

--- TTKTest/ttkautotest.h
+++ TTKTest/ttkautotest.h
@@ -1,6 +1,14 @@
 #ifndef TTKAUTOTEST_H
 #define TTKAUTOTEST_H
 
+/* =================================================
+ * This file is part of the TTK Music Player project
+ * Copyright (c) 2014 - 2016 Greedysky Studio
+ * All rights reserved!
+ * Redistribution and use of the source code or any derivative
+ * works are strictly forbiden.
+   =================================================*/
+
 #include <QTest>
 #include <QDebug>
 #include <QObject>
--- TTKUpdate/musicZip/gojobzip.h
+++ TTKUpdate/musicZip/gojobzip.h
@@ -1,6 +1,14 @@
 #ifndef GOJOBZIP_H
 #define GOJOBZIP_H
 
+/* =================================================
+ * This file is part of the TTK Music Player project
+ * Copyright (c) 2014 - 2016 Greedysky Studio
+ * All rights reserved!
+ * Redistribution and use of the source code or any derivative
+ * works are strictly forbiden.
+   =================================================*/
+
 #include <QStringList>
 #include "musicupdateglobaldefine.h"
 
