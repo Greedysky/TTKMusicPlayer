ddb269d361a830c58813a6d5ed31e6d13a061a46
Greedysky <greedysky@163.com>
2016-10-24 20:03:02 +0800

    update mobile translation[][854221]

--- TTKMobile/TTKMobile.ts
+++ TTKMobile/TTKMobile.ts
@@ -2,6 +2,29 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>MusicDownLoadQueryAlbumWYThread</name>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadqueryalbumwythread.cpp" line="120"/>
+        <source>ST</source>
+        <translation>压缩品质</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadqueryalbumwythread.cpp" line="122"/>
+        <source>SD</source>
+        <translation>标准品质</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadqueryalbumwythread.cpp" line="124"/>
+        <source>HD</source>
+        <translation>高品质</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadqueryalbumwythread.cpp" line="126"/>
+        <source>SQ</source>
+        <translation>超高品质</translation>
+    </message>
+</context>
+<context>
     <name>MusicDownLoadQueryMultipleThread</name>
     <message>
         <location filename="../TTKCore/musicNetwork/musicdownloadquerymultiplethread.cpp" line="148"/>
@@ -25,8 +48,51 @@
     </message>
 </context>
 <context>
+    <name>MusicDownLoadQueryWYThread</name>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadquerywythread.cpp" line="154"/>
+        <source>ST</source>
+        <translation>压缩品质</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadquerywythread.cpp" line="156"/>
+        <source>SD</source>
+        <translation>标准品质</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadquerywythread.cpp" line="158"/>
+        <source>HD</source>
+        <translation>高品质</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicNetwork/wy/musicdownloadquerywythread.cpp" line="160"/>
+        <source>SQ</source>
+        <translation>超高品质</translation>
+    </message>
+</context>
+<context>
+    <name>MusicDownloadStatusLabel</name>
+    <message>
+        <location filename="../TTKCore/musicWidget/musicdownloadstatuslabel.cpp" line="83"/>
+        <location filename="../TTKCore/musicWidget/musicdownloadstatuslabel.cpp" line="85"/>
+        <source>TTKMusicPlayer</source>
+        <translation>天天酷音</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicWidget/musicdownloadstatuslabel.cpp" line="84"/>
+        <source>The Internet Seems To Be A Problem, Let&apos;s Listen To The Local Music.</source>
+        <translation>网络连接似乎出了点问题，先听听本地音乐吧!</translation>
+    </message>
+    <message>
+        <location filename="../TTKCore/musicWidget/musicdownloadstatuslabel.cpp" line="86"/>
+        <source>Network Connection Has Been Restored.</source>
+        <translation>网络连接已恢复!</translation>
+    </message>
+</context>
+<context>
     <name>QJson::ParserRunnable</name>
     <message>
+        <location filename="../TTKThirdParty/MusicExtras/qjson/parserrunnable.cpp" line="62"/>
         <source>An error occurred while parsing json: %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -34,20 +100,36 @@
 <context>
     <name>QObject</name>
     <message>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="124"/>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="148"/>
         <source>ss</source>
         <translation>秒前</translation>
     </message>
     <message>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="128"/>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="152"/>
         <source>mm</source>
         <translation>分钟前</translation>
     </message>
     <message>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="132"/>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="156"/>
         <source>hh</source>
         <translation>小时前</translation>
     </message>
     <message>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="136"/>
+        <location filename="../TTKCore/musicCore/musictime.cpp" line="160"/>
         <source>day</source>
         <translation>天前</translation>
+    </message>
+</context>
+<context>
+    <name>TTKMusicLyricModel</name>
+    <message>
+        <location filename="MobileCore/ttkmusiclyricmodel.cpp" line="90"/>
+        <source>No Lrc Found</source>
+        <translation>还没有歌词哦！</translation>
     </message>
 </context>
 </TS>
