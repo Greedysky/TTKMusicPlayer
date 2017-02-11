695462b79554bbe3ccc253f6cc104e481a4c9394
greedysky <greedysky@163.com>
2017-02-11 12:41:15 +0800

    Clean code[031589]

--- TTKThirdParty/MusicExtras/qaes/QAes.pri
+++ TTKThirdParty/MusicExtras/qaes/QAes.pri
@@ -1,3 +1,11 @@
+/* =================================================
+ * This file is part of the TTK Music Player project
+ * Copyright (c) 2015 - 2017 Greedysky Studio
+ * All rights reserved!
+ * Redistribution and use of the source code or any derivative
+ * works are strictly forbiden.
+   =================================================*/
+
 INCLUDEPATH += $$PWD/
 
 SOURCES += \
--- TTKThirdParty/MusicExtras/qaes/qaeswrap.h
+++ TTKThirdParty/MusicExtras/qaes/qaeswrap.h
@@ -1,6 +1,14 @@
 #ifndef QAESWRAP_H
 #define QAESWRAP_H
 
+/* =================================================
+ * This file is part of the TTK Music Player project
+ * Copyright (c) 2015 - 2017 Greedysky Studio
+ * All rights reserved!
+ * Redistribution and use of the source code or any derivative
+ * works are strictly forbiden.
+   =================================================*/
+
 #include "musicextrasglobaldefine.h"
 
 /*! @brief The namespace of the aes wrapper.
