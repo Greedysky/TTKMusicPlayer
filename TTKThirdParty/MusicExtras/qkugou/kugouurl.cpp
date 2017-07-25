218fdd9c5b6554a0d14ed47baf3311afa3c5232d
greedysky <greedysky@163.com>
2017-07-25 20:23:08 +0800

    Update kugou web version to 8173[777898]

--- TTKThirdParty/MusicExtras/qkugou/kugouurl.cpp
+++ TTKThirdParty/MusicExtras/qkugou/kugouurl.cpp
@@ -7,49 +7,49 @@ KugouUrl::KugouUrl()
 
 QString KugouUrl::getSongYuekuUrl()
 {
-    return "http://www2.kugou.kugou.com/yueku/v9/html/default.html?ver=8010";
+    return "http://www2.kugou.kugou.com/yueku/v9/html/default.html?ver=8171";
 }
 
 QString KugouUrl::getSongRecommendUrl()
 {
-    return "http://www2.kugou.kugou.com/yueku/v9/html/home.html";
+    return "http://www2.kugou.kugou.com/yueku/v9/html/home.html?ver=8171";
 }
 
 QString KugouUrl::getSongRankUrl()
 {
-    return "http://www2.kugou.kugou.com/yueku/v9/rank/home/1-22943.html";
+    return "http://www2.kugou.kugou.com/yueku/v9/rank/home/1-22943.html?ver=8171";
 }
 
 QString KugouUrl::getSongSingerUrl()
 {
-    return "http://www2.kugou.kugou.com/yueku/v9/singer/index/1-all-0-0_0.html";
+    return "http://www2.kugou.kugou.com/yueku/v9/singer/index/1-all-0-0_0.html?ver=8171";
 }
 
 QString KugouUrl::getSongCategoryUrl()
 {
-    return "http://www2.kugou.kugou.com/yueku/v9/categoryv2/index.html";
+    return "http://www2.kugou.kugou.com/yueku/v9/categoryv2/index.html?ver=8171";
 }
 
 QString KugouUrl::getSongShowUrl()
 {
-    return "http://www2.kugou.kugou.com/show/v9/html/index.html";
+    return "http://www2.kugou.kugou.com/show/v9/html/index.html?ver=8171";
 }
 
 QString KugouUrl::getSongHeroesUrl()
 {
-    return "http://huodong.kugou.com/a2016/html/HeroesOfRemix.html";
+    return "http://huodong.kugou.com/a2016/html/HeroesOfRemix.html?ver=8171";
 }
 
 
 
 QString KugouUrl::getRadioPublicUrl()
 {
-    return "http://www2.kugou.kugou.com/fm2/app/publicV2/html/recommend/index.html";
+    return "http://www2.kugou.kugou.com/fm2/app/publicV2/html/recommend/index.html?ver=8171";
 }
 
 QString KugouUrl::getRadioHighFmUrl()
 {
-    return "http://www2.kugou.kugou.com/fm2/html/highFm.html";
+    return "http://www2.kugou.kugou.com/fm2/html/highFm.html?ver=8171";
 }
 
 QString KugouUrl::getRadioFxUrl()
@@ -59,43 +59,43 @@ QString KugouUrl::getRadioFxUrl()
 
 QString KugouUrl::getRadioHomeUrl()
 {
-    return "http://ugcfm.kugou.com/fm2/html/ugc/home.html";
+    return "http://ugcfm.kugou.com/fm2/html/ugc/home.html?ver=8171";
 }
 
 
 
 QString KugouUrl::getListUrl()
 {
-    return "http://www2.kugou.kugou.com/yueku/v9/special/index/getData.js?cdn=cdn&t=5&c=";
+    return "http://www2.kugou.kugou.com/yueku/v9/special/index/getData.js?cdn=cdn&t=5&c=&ver=8171";
 }
 
 
 
 QString KugouUrl::getMVRadioUrl()
 {
-    return "http://www2.kugou.kugou.com/mv/v9/html/mvradio.html";
+    return "http://www2.kugou.kugou.com/mv/v9/html/mvradio.html?ver=8171";
 }
 
 QString KugouUrl::getMVRecommendUrl()
 {
-    return "http://www2.kugou.kugou.com/mv/v9/mtv/index/getData.js?cdn=cdn";
+    return "http://www2.kugou.kugou.com/mv/v9/mtv/index/getData.js?cdn=cdn&ver=8171";
 }
 
 QString KugouUrl::getMVFanxingUrl()
 {
-    return "http://fanxing.kugou.com/index.php?action=embedPageMv";
+    return "http://fanxing.kugou.com/index.php?action=embedPageMv&ver=8171";
 }
 
 
 
 QString KugouUrl::getWebPlayerUrl()
 {
-    return "http://web.kugou.com/index.html?";
+    return "http://web.kugou.com/index.html?ver=8171";
 }
 
 
 
 QString KugouUrl::getKuiSheUrl()
 {
-    return "http://huodong.kugou.com/2014/html/kuishe.html?ver=8090";
+    return "http://huodong.kugou.com/2014/html/kuishe.html?ver=8171";
 }
