cca920248a32e8c642a63313d13f62fd8134d31a
Greedysky <greedysky@163.com>
2019-01-08 21:43:41 +0800

    Fixed[948722]

--- TTKRun/TTKApp/ttklocalpeer.cpp
+++ TTKRun/TTKApp/ttklocalpeer.cpp
@@ -115,7 +115,7 @@ bool TTKLocalPeer::isClient()
         return true;
     }
 
-    const bool res = d->m_server->listen(d->m_socketName);
+    bool res = d->m_server->listen(d->m_socketName);
 #if defined(Q_OS_UNIX) && (QT_VERSION >= QT_VERSION_CHECK(4,5,0))
     // ### Workaround
     if(!res && d->m_server->serverError() == QAbstractSocket::AddressInUseError)
