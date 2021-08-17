4be20378a4f99d62f3021de99139fe8271db5b43
greedysky <greedysky@163.com>
2021-08-17 10:22:19 +0800

    refactor: Update timestamp query url[013017]

--- TTKScript/make_cert.bat
+++ TTKScript/make_cert.bat
@@ -6,6 +6,6 @@ set base=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\bin
 "%base%\makecert.exe" -a sha512 -r -n "CN=Greedysky Studio" -b 01/01/2016 -e 12/31/2022 -sv %temp%\CbipSetupKey.pvk %temp%\CbipSetupKey.cer
 "%base%\cert2spc.exe" %temp%\CbipSetupKey.cer %temp%\CbipSetupKey.spc
 "%base%\pvk2pfx.exe" -pvk %temp%\CbipSetupKey.pvk -spc %temp%\CbipSetupKey.spc -f -pfx %userprofile%\Desktop\TTKMusicplayer.pfx
-"%base%\signtool.exe" sign /f %userprofile%\Desktop\TTKMusicplayer.pfx  /t "http://timestamp.verisign.com/scripts/timstamp.dll" /d "TTKMusicplayer" %1
+"%base%\signtool.exe" sign /f %userprofile%\Desktop\TTKMusicplayer.pfx  /t "http://time.certum.pl/" /d "TTKMusicplayer" %1
 del %temp%\CbipSetupKey.* >nul 2>nul
 del %userprofile%\Desktop\TTKMusicplayer.pfx >nul 2>nul
