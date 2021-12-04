1c10e453df47521d1ff6051ff52294e44aa2c024
Greedysky <greedysky@163.com>
2021-12-04 10:46:31 +0800

    refactor: Update application module copyright[574829]

--- qmmp/src/plugins/Input/zed/mdx/decodermdxfactory.h
+++ qmmp/src/plugins/Input/zed/mdx/decodermdxfactory.h
@@ -1,6 +1,6 @@
-/* =================================================
+/***************************************************************************
  * This file is part of the TTK qmmp plugin project
- * Copyright (C) 2015 - 2021 Greedysky Studio
+ * Copyright (C) 2015 - 2022 Greedysky Studio
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 
  * You should have received a copy of the GNU General Public License along
  * with this program; If not, see <http://www.gnu.org/licenses/>.
- ================================================= */
+ ***************************************************************************/
 
 #ifndef DECODERMDXFACTORY_H
 #define DECODERMDXFACTORY_H
