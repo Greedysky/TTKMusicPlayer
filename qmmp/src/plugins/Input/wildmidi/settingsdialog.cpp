533a4fcc77f415c9b805c0a66569ed740077cfaa
Greedysky <greedysky@163.com>
2022-04-17 17:12:21 +0800

    feat: Add config path selection[250312]

--- qmmp/src/plugins/Input/wildmidi/settingsdialog.cpp
+++ qmmp/src/plugins/Input/wildmidi/settingsdialog.cpp
@@ -19,11 +19,10 @@ SettingsDialog::SettingsDialog(QWidget *parent)
 #endif
     QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
     settings.beginGroup("Midi");
+    m_ui.configPathEdit->setText(settings.value("conf_path").toString());
     m_ui.sampleRateComboBox->addItem(tr("44100 Hz"), 44100);
     m_ui.sampleRateComboBox->addItem(tr("48000 Hz"), 48000);
-
-    int i = m_ui.sampleRateComboBox->findData(settings.value("sample_rate", 44100).toInt());
-    m_ui.sampleRateComboBox->setCurrentIndex(i);
+    m_ui.sampleRateComboBox->setCurrentIndex(m_ui.sampleRateComboBox->findData(settings.value("sample_rate", 44100).toInt()));
     m_ui.enhancedResamplingCheckBox->setChecked(settings.value("enhanced_resampling", false).toBool());
     m_ui.reverbCheckBox->setChecked(settings.value("reverberation", false).toBool());
     settings.endGroup();
@@ -33,6 +32,7 @@ void SettingsDialog::accept()
 {
     QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
     settings.beginGroup("Midi");
+    settings.setValue("conf_path", m_ui.configPathEdit->text());
     settings.setValue("sample_rate", m_ui.sampleRateComboBox->itemData(m_ui.sampleRateComboBox->currentIndex()));
     settings.setValue("enhanced_resampling", m_ui.enhancedResamplingCheckBox->isChecked());
     settings.setValue("reverberation", m_ui.reverbCheckBox->isChecked());
