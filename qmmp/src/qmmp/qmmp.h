803af41c67bd4cdbc683a3b84cb6e5b48c30fa79
greedysky <greedysky@163.com>
2023-03-17 23:53:05 +0800

    refactor: Clean code[846850]

--- qmmp/src/qmmp/qmmp.h
+++ qmmp/src/qmmp/qmmp.h
@@ -57,7 +57,7 @@
 #  if QT_VERSION < QT_VERSION_CHECK(5,7,0)
 // this adds const to non-const objects (like std::as_const)
 template <typename T>
-Q_DECL_CONSTEXPR typename std::add_const<T>::type &qAsConst(T &t) { return t; }
+Q_DECL_CONSTEXPR typename std::add_const<T>::type &qAsConst(T &t) noexcept { return t; }
 // prevent rvalue arguments:
 template <typename T>
 void qAsConst(const T &&) = delete;
