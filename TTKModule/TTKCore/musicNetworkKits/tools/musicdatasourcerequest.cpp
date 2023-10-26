8d06b71ee83d96223f26dbab9a290fe133960b9e
Greedysky <greedysky@163.com>
2023-10-26 22:20:03 +0800

    docs: Update resource document[295320]

--- TTKModule/TTKCore/musicNetworkKits/tools/musicdatasourcerequest.cpp
+++ TTKModule/TTKCore/musicNetworkKits/tools/musicdatasourcerequest.cpp
@@ -17,6 +17,7 @@ void MusicDataSourceRequest::startRequest(const QString &url)
     QNetworkRequest request;
     request.setUrl(url);
     TTK::setSslConfiguration(&request);
+    TTK::makeUserAgentHeader(&request);
     TTK::makeContentTypeHeader(&request);
 
     m_reply = m_manager.get(request);
