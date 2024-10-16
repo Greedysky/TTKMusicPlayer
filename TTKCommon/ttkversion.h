0f75dc73ae5ff1fa3053926479f916816677e1c4
greedysky <greedysky@163.com>
2024-10-16 10:09:01 +0800

    Bump version 4.1.0.0 Final

--- TTKCommon/ttkversion.h
+++ TTKCommon/ttkversion.h
@@ -162,16 +162,16 @@
  * with this program; If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
 
-//update time 2024.06.18
+//update time 2024.10.16
 #define TTK_MAJOR_VERSION 4
-#define TTK_MINOR_VERSION 0
+#define TTK_MINOR_VERSION 1
 #define TTK_PATCH_VERSION 0
 
 #define TTK_CONFIG_VERSION_STR  "1.0.2"
 
 #define TTK_VERSION             TTK_VERSION_CHECK(TTK_MAJOR_VERSION, TTK_MINOR_VERSION, TTK_PATCH_VERSION, 0)
 #define TTK_VERSION_STR         TTK_VERSION_CHECK_STR(TTK_MAJOR_VERSION, TTK_MINOR_VERSION, TTK_PATCH_VERSION, 0)
-#define TTK_VERSION_TIME_STR    "(2024/06/18)"
+#define TTK_VERSION_TIME_STR    "(2024/10/16)"
 
 #define TTK_RC_FILEVERSION      TTK_MAJOR_VERSION, TTK_MINOR_VERSION, TTK_PATCH_VERSION, 0
 #define TTK_RC_PRODUCTVERSION   TTK_VERSION_STR
