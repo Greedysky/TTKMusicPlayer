19c3a6c2d921f5268959ece1e263fa9789c29ac5
Greedysky <greedysky@163.com>
2025-05-14 16:37:23 +0800

    refactor: Clean code[792201]

--- qmmp/src/qmmp/qmmp.h
+++ qmmp/src/qmmp/qmmp.h
@@ -56,17 +56,6 @@
 
 #ifndef TTK_LIBRARY
 // as_const defined
-#if QT_VERSION < QT_VERSION_CHECK(5,7,0)
-// this adds const to non-const objects (like std::as_const)
-template <typename T>
-Q_DECL_CONSTEXPR typename std::add_const<T>::type &qAsConst(T &t) noexcept { return t; }
-// prevent rvalue arguments:
-template <typename T>
-void qAsConst(const T &&) = delete;
-#elif QT_VERSION >= QT_VERSION_CHECK(6,6,0)
-#  define qAsConst std::as_const
-#endif
-
 #if __cplusplus < 201703L
 namespace std
 {
@@ -77,6 +66,10 @@ Q_DECL_CONSTEXPR typename std::add_const<T>::type &as_const(T &t) noexcept { ret
 template <typename T>
 void as_const(const T &&) = delete;
 }
+#endif
+
+#if QT_VERSION < QT_VERSION_CHECK(5,7,0) || QT_VERSION >= QT_VERSION_CHECK(6,6,0)
+#  define qAsConst std::as_const
 #endif
 
 #if QT_VERSION < QT_VERSION_CHECK(5,10,0)
