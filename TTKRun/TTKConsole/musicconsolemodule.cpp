d0aa111282a51f1b969ce46c0782685172bb2a70
Greedysky <greedysky@163.com>
2025-07-18 16:12:05 +0800

    refactor: Clean code[120658]

--- TTKRun/TTKConsole/musicconsolemodule.cpp
+++ TTKRun/TTKConsole/musicconsolemodule.cpp
@@ -67,7 +67,7 @@ MusicConsoleModule::~MusicConsoleModule()
 
 bool MusicConsoleModule::initialize()
 {
-    QString text = TTK_LINEFEED TTK_APP_NAME "Console Module" "v" TTK_VERSION_STR TTK_LINEFEED;
+    QString text = QString("\n%1 Console Module v%2\n").arg(TTK_APP_NAME, TTK_VERSION_STR);
             text += "Offical web page: https://github.com/Greedysky/TTKMusicPlayer\n";
             text += "Copyright(C) 2015 - 2025 Greedysky All Rights Reserved\n";
             text += "TTKMusicPlayer imitates Kugou UI, the music player uses of qmmp core library based on Qt for windows and linux\n\n";
