414c8c92eb15a3824a6be675cc81e08f3d4f61b5
Greedysky <greedysky@163.com>
2025-07-31 13:34:44 +0800

    docs: Update resource document[923144]

--- TTKUtils/ts_win.bat
+++ TTKUtils/ts_win.bat
@@ -6,15 +6,18 @@ set OUTPUT="%2/GLanguage"
 set SOURCE="%3/TTKLanguage"
 
 ::make output dir
-if not exist "%OUTPUT%" md "%OUTPUT%"
+if not exist %OUTPUT% md %OUTPUT%
 
 ::make
-for /r "%SOURCE%" %%f in (*.ts) do %LCEXE% "%%f"
+for /r %SOURCE% %%f in (*.ts) do %LCEXE% %%f
 
 ::rename
-for /r "%SOURCE%" %%f in (*.qm) do ren "%%f" *.ln
+for /r %SOURCE% %%f in (*.qm) do ren %%f *.ln
 
 ::move
-for /r "%SOURCE%" %%f in (*.ln) do move /y "%%f" "%OUTPUT%"
+for /r %SOURCE% %%f in (*.ln) do move /y %%f %OUTPUT%
+
+echo translate finished!
+echo =============================================================================
 
 POPD %~dp0
