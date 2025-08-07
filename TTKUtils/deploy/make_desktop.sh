361db4c930db9c43fc77d4607957656c53115c8e
Greedysky <greedysky@163.com>
2025-08-07 10:33:03 +0800

    docs: Update resource document[153301]

--- TTKUtils/deploy/make_desktop.sh
+++ TTKUtils/deploy/make_desktop.sh
@@ -7,11 +7,6 @@ if [ "${dirpath%${tmp}}" != "/" ]; then
   dirpath=$PWD/${dirpath}
 fi
 
-deskpath=${dirpath}/share/applications
-if [ ! -d ${deskpath} ]; then
-  mkdir -p ${deskpath}
-fi
-
 username=`logname`
 if [ "${username}" = "root" ]; then
   homepath=/root
@@ -20,7 +15,7 @@ else
 fi
 
 packvern=4.2.0.0
-packpath=${deskpath}/TTKMusicPlayer.desktop
+packpath=${dirpath}/TTKMusicPlayer.desktop
 userpath=${homepath}/.local/share/applications
 iconpath="${dirpath}/share/pixmaps/ttkmusicplayer.png"
 execpath="${dirpath}/../TTKMusicPlayer"
@@ -45,6 +40,5 @@ Categories=AudioVideo;Player;Audio;Qt;
 MimeType=application/x-ogg;audio/mp3;audio/mpeg;audio/flac;audio/x-flac;audio/opus;audio/x-mp3;audio/x-mpeg;audio/x-ms-wma;audio/x-musepack;audio/x-tta;audio/x-wavpack;application/ogg;audio/x-vorbis+ogg;audio/x-scpls;audio/x-mpegurl;audio/x-it;audio/x-mod;audio/x-s3m;audio/x-stm;audio/x-xm;audio/x-ape;application/x-cue;x-content/audio-cdda;audio/x-ffmpeg-shorten;audio/aac;audio/aacp;audio/x-aac;audio/m4a;audio/x-m4a;audio/aiff;audio/basic;audio/mid;audio/mp2;audio/mp4;audio/wav;audio/3gpp;audio/3gpp2;audio/vnd.rn-realaudio;audio/ac3;audio/eac3;audio/dts;audio/true-hd;audio/x-matroska;audio/speech;audio/webm;
 X-KDE-StartupNotify=false\n" > ${packpath}
 
-cp -rv ${packpath} ${userpath}
-rm -R ${deskpath}
+mv -fv ${packpath} ${userpath}
 chmod +x ${userpath}
