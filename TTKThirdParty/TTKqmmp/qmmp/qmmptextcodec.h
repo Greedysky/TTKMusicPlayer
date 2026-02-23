91a90b3b5b4baeebcff0ef04e31de5b23929924b
Greedysky <greedysky@163.com>
2026-02-23 10:04:08 +0800

    docs: Update copyright resource document[966772]

--- TTKThirdParty/TTKqmmp/qmmp/qmmptextcodec.h
+++ TTKThirdParty/TTKqmmp/qmmp/qmmptextcodec.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2021-2025 by Ilya Kotov                                 *
+ *   Copyright (C) 2021-2026 by Ilya Kotov                                 *
  *   forkotov02@ya.ru                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
