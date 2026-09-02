c674cf86cf541c9ad0ce7236cdde69bcdd3b5f39
Greedysky <greedysky@163.com>
2026-09-02 16:19:28 +0800

    refactor: Optimize pv counter network module[835112]

--- TTKModule/TTKCore/musicNetworkKits/tools/musicpvcounterrequest.cpp
+++ TTKModule/TTKCore/musicNetworkKits/tools/musicpvcounterrequest.cpp
@@ -2,7 +2,7 @@
 
 #include "qsync/qsyncutils.h"
 
-static constexpr const char *QUERY_COUNTER_URL = "counter";
+static constexpr const char *QUERY_COUNTERS_URL = "counters";
 
 MusicPVCounterRequest::MusicPVCounterRequest(QObject *parent)
     : MusicAbstractNetwork(parent)
@@ -13,38 +13,68 @@ MusicPVCounterRequest::MusicPVCounterRequest(QObject *parent)
 void MusicPVCounterRequest::startToRequest()
 {
     QByteArray bytes;
-    QFile file(RESOURCE_DIR_FULL + QUERY_COUNTER_URL);
+    QFile file(RESOURCE_DIR_FULL + QUERY_COUNTERS_URL);
     if(file.open(QIODevice::ReadOnly))
     {
-        TTK_INFO_STREAM("PV counter using local resource config");
+        TTK_INFO_STREAM("PV counters using local resource config");
         bytes = file.readAll();
         file.close();
     }
     else
     {
-        TTK_INFO_STREAM("PV counter using network resource config");
+        TTK_INFO_STREAM("PV counters using network resource config");
         QNetworkRequest request;
-        request.setUrl(QSyncUtils::makeDataBucketUrl() + RESOURCE_DIR + QUERY_COUNTER_URL);
+        request.setUrl(QSyncUtils::makeDataBucketUrl() + RESOURCE_DIR + QUERY_COUNTERS_URL);
         TTK::setUserAgentHeader(&request);
         bytes = TTK::syncNetworkQueryForGet(&request);
     }
 
     if(bytes.isEmpty())
     {
-        TTK_ERROR_STREAM("Counter PV data error");
+        TTK_ERROR_STREAM("Counters PV data error");
         Q_EMIT downloadDataChanged(TTK_DEFAULT_STR);
         return;
     }
 
-    QNetworkRequest request;
-    request.setUrl(QString::fromUtf8(bytes));
-    TTK::setUserAgentHeader(&request);
-    TTK::setSslConfiguration(&request);
-    TTK::setContentTypeHeader(&request);
+    QJsonParseError ok;
+    const QJsonDocument &json = QJsonDocument::fromJson(bytes, &ok);
+    if(QJsonParseError::NoError == ok.error)
+    {
+        const QVariantList &servers = json.toVariant().toList();
+        for(const QVariant &var : qAsConst(servers))
+        {
+            if(var.isNull())
+            {
+                continue;
+            }
 
-    m_reply = m_manager.get(request);
-    connect(m_reply, SIGNAL(finished()), SLOT(downloadFinished()));
-    QtNetworkErrorConnect(m_reply, this, replyError, TTK_SLOT);
+            const QVariantMap &server = var.toMap();
+            const bool option = server["option"].toBool();
+
+            if(!option)
+            {
+                continue;
+            }
+
+            QNetworkRequest request;
+            request.setUrl(TTK::Algorithm::mdII(server["base"].toString(), false));
+            TTK::setUserAgentHeader(&request);
+            TTK::setSslConfiguration(&request);
+            TTK::setContentTypeHeader(&request);
+
+            if(server.contains("body"))
+            {
+                m_reply = m_manager.post(request, TTK::Algorithm::mdII(server["body"].toString(), false).toUtf8());
+            }
+            else
+            {
+                m_reply = m_manager.get(request);
+            }
+
+            connect(m_reply, SIGNAL(finished()), SLOT(downloadFinished()));
+            QtNetworkErrorConnect(m_reply, this, replyError, TTK_SLOT);
+        }
+    }
 }
 
 void MusicPVCounterRequest::downloadFinished()
